cbf6c556f3859c6faa2cad60b331f942aa91f8bb
Joseph Chingalo <profschingalo@gmail.com>
2019-07-04 16:07:29 +0300

    Add list of error list

--- app/src/main/java/com/example/android/androidskeletonapp/ui/d2_errors/D2ErrorActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/d2_errors/D2ErrorActivity.java
@@ -1,9 +1,16 @@
 package com.example.android.androidskeletonapp.ui.d2_errors;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.paging.PagedList;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
+
+import org.hisp.dhis.android.core.maintenance.D2Error;
 
 public class D2ErrorActivity extends ListActivity {
 
@@ -11,6 +18,21 @@ public class D2ErrorActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setUp(R.layout.activity_d2_errors, R.id.d2ErrorsToolbar, R.id.d2ErrorsRecyclerView);
-        // TODO List D2 errors
+        observeError();
+    }
+
+    private void observeError(){
+        D2ErrorAdapter adapter = new D2ErrorAdapter();
+        recyclerView.setAdapter(adapter);
+
+
+        LiveData<PagedList<D2Error>> errorPagedListLiveData = Sdk.d2().maintenanceModule().d2Errors.getPaged(10);
+
+        errorPagedListLiveData.observe(this,errorPagedList->{
+            adapter.submitList(errorPagedList);
+            findViewById(R.id.d2ErrorsNotificator).setVisibility(
+                    errorPagedList.isEmpty() ? View.VISIBLE : View.GONE
+            );
+        });
     }
 }
--- app/src/main/java/com/example/android/androidskeletonapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/main/MainActivity.java
@@ -10,6 +10,7 @@ import com.example.android.androidskeletonapp.R;
 import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.data.service.ActivityStarter;
 import com.example.android.androidskeletonapp.data.service.SyncStatusHelper;
+import com.example.android.androidskeletonapp.ui.d2_errors.D2ErrorActivity;
 import com.example.android.androidskeletonapp.ui.programs.ProgramsActivity;
 import com.example.android.androidskeletonapp.ui.tracked_entity_instances.TrackedEntityInstancesActivity;
 import com.example.android.androidskeletonapp.ui.tracked_entity_instances.search.TrackedEntityInstanceSearchActivity;
@@ -270,6 +271,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             wipeData();
         } else if (id == R.id.navExit) {
             compositeDisposable.add(logOut(this));
+        }else if(id == R.id.navErrorList){
+            ActivityStarter.startActivity(this, D2ErrorActivity.class,false);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawerLayout);
